79b6f40ae225dd1ab6a67fd2372ab5a9ef572850
amboob223 <monk8896@gmail.com>
2024-03-13 06:54:15 -0400

    hhh

---
+++ feb/regalsouls/server/sql.sql
@@ -0,0 +1,9 @@
+CREATE TABLE readings(
+     id SERIAL PRIMARY KEY,
+     first VARCHAR(255),
+     last VARCHAR(255),
+     email VARCHAR(255),
+     phone VARCHAR(255),
+     birthdate VARCHAR(255),
+     target VARCHAR(255)
+)
